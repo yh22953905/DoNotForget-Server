0adee100ba781691fba99a23dfb0c4e0ee7bf068
yh22953905 <yh22953905@gmail.com>
2021-06-07 00:05:18 +0900

    - gradle default 경로에 따라 index.adoc 파일 경로 변경 - asciidoctor 에 필요한 의존성 추가 - 생성된 REST DOCS 에 대한 Security 설정 수정

--- src/main/java/com/hungrybrothers/alarmforsubscription/security/SecurityConfig.java
+++ src/main/java/com/hungrybrothers/alarmforsubscription/security/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/docs/index.html");
         web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
