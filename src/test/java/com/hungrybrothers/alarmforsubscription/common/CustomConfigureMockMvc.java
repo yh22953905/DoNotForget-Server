eaa4b77cde7241803f6f6bd4555ae7cd7a2e8284
yh22953905 <yh22953905@gmail.com>
2021-09-20 13:52:46 +0900

    fix: mockmvc results encoding

---
+++ src/test/java/com/hungrybrothers/alarmforsubscription/common/CustomConfigureMockMvc.java
@@ -0,0 +1,27 @@
+package com.hungrybrothers.alarmforsubscription.common;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@AutoConfigureMockMvc
+@Import({
+	CustomConfigureMockMvc.CustomConfigs.class
+})
+public @interface CustomConfigureMockMvc {
+	class CustomConfigs {
+		@Bean
+		MockMvcBuilderCustomizer mockMvcBuilderCustomizer() {
+			return builder -> builder.addFilter(new CharacterEncodingFilter("UTF-8", true));
+		}
+	}
+}
