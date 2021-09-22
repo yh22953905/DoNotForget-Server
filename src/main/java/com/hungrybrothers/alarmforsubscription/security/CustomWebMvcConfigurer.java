4da8c752e2865163a1a02c07fe9891e7740d6eec
yh22953905 <yh22953905@gmail.com>
2021-09-22 14:40:22 +0900

    feat: CORS Config

---
+++ src/main/java/com/hungrybrothers/alarmforsubscription/security/CustomWebMvcConfigurer.java
@@ -0,0 +1,15 @@
+package com.hungrybrothers.alarmforsubscription.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class CustomWebMvcConfigurer implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*");
+	}
+}
